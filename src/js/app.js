7726dff8c01a398c0a6220a3b65de1150c1035e5
NikitaKST <8172535@gmail.com>
2023-10-15 19:19:55 +0500

    Task complete

---
+++ src/js/app.js
@@ -0,0 +1,34 @@
+/* eslint-disable no-restricted-syntax */
+/* eslint-disable indent */
+export default function orderByProps(obj, arr = []) {
+  const result = [];
+  const keys = [];
+
+arr.forEach((element) => {
+  for (const key in obj) {
+    if (key === element) {
+      result.push({
+        key: `${key}`,
+        value: `${obj[key]}`,
+      });
+    }
+  }
+});
+
+for (const key in obj) {
+  if (!arr.includes(key)) {
+    keys.push(key);
+  }
+}
+
+  keys.sort();
+
+  keys.forEach((element) => {
+    result.push({
+      key: `${element}`,
+      value: `${obj[element]}`,
+    });
+  });
+
+  return result;
+}
